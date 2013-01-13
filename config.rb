ea9f97037e7c4be761d4f10d965d4434d31357c3
Lukas Kubanek <lukas@kubanek.org>
2013-01-13 16:59:55 +0100

    Removed a reference to Susy and activated CSS and JS minification

--- config.rb
+++ config.rb
@@ -2,9 +2,6 @@
 # Compass
 ###
 
-# Susy grids in Compass
-require 'susy'
-
 # Compass recipes
 require 'compass-recipes'
 
@@ -48,10 +45,10 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :cache_buster
